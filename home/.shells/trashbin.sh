64e9b999df89c13fdc1d51acc155cdca9b99b73a
Trickyni-V56 <trickyniv56@gmail.com>
2024-12-14 12:57:32 +0200

    more palette work

--- home/.shells/trashbin.sh
+++ home/.shells/trashbin.sh
@@ -2,12 +2,12 @@
 
 FILENAME=${1:-$(fzf)}
 if [ -z "$FILENAME" ]; then
-    echo -e "\e[36m«TRASHING ABORTED»\e[0m" >&2
+    echo -e "\e[90m«TRASHING ABORTED»\e[0m" >&2
     exit 1
 fi
 if [ -e "$FILENAME" ]; then
     trash "$FILENAME"
     echo -e "\e[31m \e[9m$FILENAME\e[0m"
 else
-    echo -e "\e[36m«\e[31m$FILENAME \e[36mDOES NOT EXIST»\e[0m" >&2
+    echo -e "\e[90m«\e[31m$FILENAME \e[90mDOES NOT EXIST»\e[0m" >&2
 fi
