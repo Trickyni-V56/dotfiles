295eefcd2cec0930bde5107002b0a64c22b71333
Trickyni-V56 <trickyniv56@gmail.com>
2025-05-12 13:49:27 +0300

    0

--- home/.shells/suntime.sh
+++ home/.shells/suntime.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
-coordinates=$(cat ~/.location) # add a ~/.location file with your coordinates in a <XX.XXXXXN XX.XXXXXXXE> format
+if [ ! -e '~/.location' ]; then
+    coordinates=$(cat ~/.location)
+else
+    echo "<~/.location> file not found. please create one with coordinates in a <XX.XXXXXN XX.XXXXXXXE> format" >&2
+    exit 1
+fi
 
 if [ "$(sunwait poll $coordinates)" == DAY ]; then
-    output=$(pdd $(sunwait list set $coordinates civil)":00" | head -c5)
+    sun=$(sunwait list set $coordinates civil)
+    now=$(date '+%H:%M')
+    output=$(qalc -t $sun - $now to time)
     echo "+"$output
 else
     echo "-"
