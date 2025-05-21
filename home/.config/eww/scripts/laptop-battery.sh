f37c3a8a0936b41c02f9f61d63c63ec5aa82c47b
Trickyni-V56 <trickyniv56@gmail.com>
2025-05-21 09:12:10 +0300

    0

--- home/.config/eww/scripts/laptop-battery.sh
+++ home/.config/eww/scripts/laptop-battery.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-acpi 2>/dev/null | grep -o '[0-9]\+%' || echo 404
+# acpi 2>/dev/null | grep -o '[0-9]\+%' | tr -d '%' || echo 404
+
+
+battery=$(acpi 2>/dev/null | grep -o '[0-9]\+%' | tr -d '%')
+battery=${battery:-404}
+echo $battery
 
 
 # TODO: add an if condition that adds +1000 if thing is charging
