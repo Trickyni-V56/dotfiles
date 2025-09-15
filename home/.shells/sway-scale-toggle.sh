6556b65ac1384d548e26fa213c13deef83eeae40
trickyni <trickyniv56@gmail.com>
2025-09-15 09:57:42 +0300

    0

--- home/.shells/sway-scale-toggle.sh
+++ home/.shells/sway-scale-toggle.sh
@@ -4,14 +4,19 @@ case "$1" in
       current_scale=$(swaymsg -t get_outputs | grep '"scale": '| sed -n 's/[^0-9.]*\([0-9]\+\(\.[0-9]\+\)\?\).*/\1/p')
       if [ "$current_scale" == 1.5 ]; then
         swaymsg 'output * scale 1.0'
+        qs ipc call panel scale 1.5 #values are inverse because quickshell's default is 1.5 scaled. might change later.
       else
         swaymsg 'output * scale 1.5'
+        qs ipc call panel scale 1.0
       fi
     ;;
     gameon)
         swaymsg 'output * scale 1.0'
+        qs ipc call panel scale 1.5
+
     ;;
     gameoff)
         swaymsg 'output * scale 1.5'
+        qs ipc call panel scale 1.0
     ;;
 esac
