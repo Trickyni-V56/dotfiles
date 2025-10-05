9b841b90bd7e2929bb772b084d6262edc4990b26
trickyni <trickyniv56@gmail.com>
2025-10-05 11:03:30 +0300

    0

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,26 +1,4 @@
--- Movement remap in normal mode
---vim.keymap.set('n', 'o', 'k', { noremap = true })
---vim.keymap.set('n', 'k', 'h', { noremap = true })
---vim.keymap.set('n', 'l', 'j', { noremap = true })
---vim.keymap.set('n', ';', 'l', { noremap = true })
+-- init.lua
+vim.g.mapleader = " "
+vim.g.have_nerd_font
 
--- Movement remap in visual mode
---vim.keymap.set('v', 'o', 'k', { noremap = true })
---vim.keymap.set('v', 'k', 'h', { noremap = true })
---vim.keymap.set('v', 'l', 'j', { noremap = true })
---vim.keymap.set('v', ';', 'l', { noremap = true })
-
--- Remapping O's functionality back unto J'
---vim.keymap.set('n', 'j', 'o', { noremap = true })
---vim.keymap.set('v', 'j', 'o', { noremap = true })
-
--- Ctrl-Backspace: delete word before cursor
---vim.api.nvim_set_keymap("i", "<C-H>", "<C-W>", { noremap = true })
-
--- Ctrl-Delete: delete word after cursor
---vim.api.nvim_set_keymap("i", "<C-Del>", "<C-o>dw", { noremap = true })
-
---vim.keymap.set('n', '<C-Up>', 'gg', { noremap = true })
---vim.keymap.set('n', '<C-Down>', 'G', { noremap = true })
---vim.keymap.set('n', '<C-Left>', '^', { noremap = true })
---vim.keymap.set('n', '<C-Right>', '$', { noremap = true })
