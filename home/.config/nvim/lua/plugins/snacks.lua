c6f8745fadc0c455cdc9a54245b012ecdc07376b
trickyni <trickyniv56@gmail.com>
2025-10-09 10:53:04 +0300

    0

---
+++ home/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,16 @@
+-- lazy.nvim
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    explorer = {
+      -- your explorer configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+  keys = {
+      { "<Bslash>", function() Snacks.explorer() end, desc = "File Explorer" },
+  },
+}
+
