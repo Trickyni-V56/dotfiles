80c252283b954109e2e020a0f3c5718d9055baef
trickyni <trickyniv56@gmail.com>
2025-10-10 18:29:48 +0300

    0

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,7 +1,7 @@
-local vim=vim --shuts up lua LSP about the vim global variable
+local vim = vim --shuts up lua LSP about the vim global variable
 
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 require("config.lazy")
 
 vim.g.have_nerd_font = true
@@ -16,42 +16,42 @@ vim.o.showmode = false -- hides mode indicator, since we have a status line
 vim.o.cursorline = true -- highlights cursor line
 vim.o.scrolloff = 10 -- keeps 10 lines above/below the cursor when scrolling
 vim.o.breakindent = true
-vim.o.signcolumn = 'yes' 
-vim.o.inccommand = 'nosplit' -- shows find/replace results live
+vim.o.signcolumn = "yes"
+vim.o.inccommand = "nosplit" -- shows find/replace results live
 vim.opt.backspace = { "start", "eol", "indent" }
 -- whitespace characters
 vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
-vim.o.winborder="none"
+vim.o.winborder = "none"
 -- sync clipboard between OS and nvim
 -- vim.schedule(function()
 --   vim.o.clipboard = 'unnamedplus'
 -- end)
 
 -- move line up/down
-vim.keymap.set('n', '<S-up>', ':m-2<CR>') 
-vim.keymap.set('n', '<S-down>', ':m+1<CR>') 
-vim.keymap.set('v', '<S-up>', ":'<,'>m '<-2<CR>gv") 
-vim.keymap.set('v', '<S-down>', ":'<,'>m '>+1<CR>gv") 
+vim.keymap.set("n", "<S-up>", ":m-2<CR>")
+vim.keymap.set("n", "<S-down>", ":m+1<CR>")
+vim.keymap.set("v", "<S-up>", ":'<,'>m '<-2<CR>gv")
+vim.keymap.set("v", "<S-down>", ":'<,'>m '>+1<CR>gv")
 
--- indent/unindent 
-vim.keymap.set('n', '<S-right>', '>>')
-vim.keymap.set('n', '<S-left>', '<<')
-vim.keymap.set('v', '<S-right>', '>gv')
-vim.keymap.set('v', '<S-left>', '<gv')
+-- indent/unindent
+vim.keymap.set("n", "<S-right>", ">>")
+vim.keymap.set("n", "<S-left>", "<<")
+vim.keymap.set("v", "<S-right>", ">gv")
+vim.keymap.set("v", "<S-left>", "<gv")
 
-vim.keymap.set('n', '<C-n>', ':enew<CR>') -- creates new buffer
-vim.keymap.set('n', '<C-]>', ':bn<CR>')
-vim.keymap.set('n', '<C-[>', ':bp<CR>')
+vim.keymap.set("n", "<C-n>", ":enew<CR>") -- creates new buffer
+vim.keymap.set("n", "<C-]>", ":bn<CR>")
+vim.keymap.set("n", "<C-[>", ":bp<CR>")
 
 -- Clear highlights on search when pressing <Esc> in normal mode
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Highlight when yanking (copying) text
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
     vim.hl.on_yank()
   end,
@@ -61,4 +61,4 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.o.background = "dark" -- or "light" for light mode
 
 -- opens diagnostic panel
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
